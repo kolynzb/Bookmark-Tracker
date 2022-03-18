18ab703877c28e473d0972d50ee9725fe6746dce
kolynzb <collinsbenda360@gmail.com>
2022-03-18 12:14:36 +0300

    Project initialization

--- components/cards/SiteCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SiteCard = () => {
-  return <div></div>;
-};
-
-export default SiteCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        jost: ["Jost"],
-      },
-      colors: {
-        accent: "#EA4335",
-        bkgBlue: "#081828",
-        bkgWhite: "#F9F9F9",
-        tranpBlue: "#3C4956",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
